188e1cc8232ad71a7c02a8b491dae353ced9dc98
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-12-03 12:12:12 +0530

    first commit

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -99,6 +99,14 @@ useEffect(()=>{
        <p className='mt-4'>Blog Description</p>
        <div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative' >
         <div ref={editorRef}></div>
+        {loading &&(
+          <div className='absolute right-0 top-0 bottom-0 left-0 flex items-center justify-center bg-black/10 mt-2'>
+            <div className='w-8 h-8 rounded-full border-2 border-t-white animate-spin'>
+
+            </div>
+
+          </div>
+        )}
         <button disabled={loading} type='button' onClick={generateContent} className='absolute bottom-1 right-2 ml-2 text-xs text-white bg-black/70 px-4 py-1.5 rounded hover:underline cursor-pointer'>
         Generate with Ai
         </button>
